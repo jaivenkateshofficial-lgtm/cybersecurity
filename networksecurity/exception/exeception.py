1a261975c1a40b8b7f296088c10ed72429f1e327
jaivenkateg <jaivenkateg@kpit.com>
2025-12-03 01:11:26 +0530

    exception handling and logging is completed

---
+++ networksecurity/exception/exeception.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from networksecurity.logging.logger import logging
+
+class NetworksecurityException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        _,_,error_tb=error_details.exc_info()
+        self.file_name=error_tb.tb_frame.f_code.co_filename
+        self.line_no=error_tb.tb_lineno
+        self.error_message=error_message
+
+    def __str__(self):
+        return f"The error occured on the file :{self.file_name} in the line number :{self.line_no} and error message is {self.error_message}"
+    
